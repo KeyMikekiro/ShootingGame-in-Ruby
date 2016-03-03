08e8abaaf7e18827db33474f7e476b0cd03b699e
Katsuya.N <0yan0sune@gmail.com>
2016-03-03 17:42:05 +0900

    fix attack process.

--- Bullet.rb
+++ Bullet.rb
@@ -3,12 +3,15 @@ class BulletType
         @image = image
         @speed = speed
         @reload_time = reload_time
+        @status = status
     end
-    attr_reader :reload_time, :image, :speed
+    attr_reader :reload_time, :image, :speed, :status
 end
 
 class Bullet < UnitObject
     def initialize( x, y, bullet_type)
         super( x, y, bullet_type.image, bullet_type.speed)
+        @status = bullet_type.status
     end
+    attr_reader :status
 end
