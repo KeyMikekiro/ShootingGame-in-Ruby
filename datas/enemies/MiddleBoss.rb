7fa40536a60713c57004fd9c14a9948a68a0086d
Katsuya.N <0yan0sune@gmail.com>
2016-03-06 01:04:45 +0900

    add middleboss action.

--- datas/enemies/MiddleBoss.rb
+++ datas/enemies/MiddleBoss.rb
@@ -1,4 +1,22 @@
+module MiddleBossForm
+    First = :first
+    Second = :second
+    Third = :third
+end
 class MiddleBoss < Enemy
+    def initialize()
+        middle_boss_speed = { :x=>0, :y=>1}
+        middle_boss_status = { :hp=>150, :attack=>10, :point=>200}
+        middle_boss_image = Resource.image("Middle_BOSS")
+        
+        @form = MiddleBossForm::First
+        
+        super( GameWindow.center_horizontal - middle_boss_image.width / 2, 
+            GameWindow.y, middle_boss_image, middle_boss_speed, middle_boss_status)
+            
+        @init_speed = @speed
+    end
+
     def moving_pattern()
         if Window.height / 2 < @sprite.y then
             @speed[:y] = 0
@@ -12,7 +30,33 @@ class MiddleBoss < Enemy
         action()
     end
     
-    def action()
+    
+    def draw()
+        super()
         GameWindow.draw_font( position_width, @sprite.y, "Test Test.", Fonts::Middle) if @status[:hp] < 50
+    end
+    
+    
+    def action()
+        if @status[:hp] <= 0 then
+            case @form
+            when MiddleBossForm::First then
+                second_form()
+            when MiddleBossForm::Second then
+                third_form()
+            end
+        end
+    end
+    
+    def second_form
+        @status[:hp] = 400
+        @sprite.image = Resource.image("Middle_BOSS_2")
+        @form = MiddleBossForm::Second
+    end
+    
+    def third_form
+        @status[:hp] = 600
+        @sprite.image = Resource.image("Middle_BOSS_3")
+        @form = MiddleBossForm::Third
     end
 end
--- datas/stages/Stage.rb
+++ datas/stages/Stage.rb
@@ -3,18 +3,14 @@ class Stage < BaseStage
     def event
         #ここにボスの出現処理とか書いてみる。
         if Score.get() >= 200 && !@middle_boss_flag then
+            puts "event enemy is attacking!!!"
             @event_enemies.push( setup_middle_boss)
         end
     end
     
     def setup_middle_boss()
-        middle_boss_speed = { :x=>0, :y=>1}
-        middle_boss_status = { :hp=>150, :attack=>10, :point=>200}
-        middle_boss_image = Resource.image("Middle_BOSS")
         
         @middle_boss_flag = true
-        
-        return MiddleBoss.new( GameWindow.center_horizontal - middle_boss_image.width / 2, 
-            GameWindow.y, middle_boss_image, middle_boss_speed, middle_boss_status)
+        return MiddleBoss.new()
     end
 end
